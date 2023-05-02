d8c98167c810f7f7e06c7028fe1b4b13d2973f83
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-05-02 10:28:20 +0600

    all chef api crated succesfully

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const allChef = require('./chefData.json')
 const port = process.env.PORT || 6005;
 
 
 
 app.get('/',(req,res)=>{
     res.send('The server is running successfully')
+})
+app.get('/allChef',(req,res)=>{
+    res.send(allChef)
+})
+app.get('/chef/:id',(req,res)=>{
+    const id = req.params.id
+    const findData = allChef.find(chef => chef.id == id)
+    res.send(findData)
 })
 
 app.listen(port,()=>{
