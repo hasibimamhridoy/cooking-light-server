8d934433f935dce1539f05a2bac132532b635423
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-05-02 10:23:43 +0600

    Server get request features added

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const port = process.env.PORT || 6005;
 
 
+
+app.get('/',(req,res)=>{
+    res.send('The server is running successfully')
+})
+
 app.listen(port,()=>{
     console.log(`The server is running and port is: ${port}`);
 })
